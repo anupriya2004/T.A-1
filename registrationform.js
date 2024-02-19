bf8ece672153c4a0003dcc025f786707baaab403
anupriya2004 <singhanupriya2008@gmail.com>
2024-02-18 23:53:37 -0800

    Initial Commit

---
+++ registrationform.js
@@ -0,0 +1,12 @@
+function registrationform(){
+    let n=document.getElementById("name").value;
+    let a=document.getElementById("address").value;
+    let e=document.getElementById("email").value;
+    let p=document.getElementById("phone").value;
+    let s=document.getElementById("subject").value;
+    console.log("NAME:",n);
+    console.log("ADDRESS:",a);
+    console.log("EMAIL ID:",e);
+    console.log("PHONE NUMBER:",p);
+    console.log("SUBJECT:",s);
+}
